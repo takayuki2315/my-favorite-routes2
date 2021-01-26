f40c54db83662993783933e87dbce79ae6bb44c6
takayuki2315 <t.s151515@icloud.com>
2021-01-26 15:13:22 +0900

    second commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
 
-        validates :nickname, presence: true
+  validates :nickname, presence: true
 
-        has_many :routes ,dependent: :destroy
-        has_many :comments, dependent: :destroy
+  has_many :routes ,dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
