a8ae896df823035decd9b037193f10b8511748fa
takayuki2315 <t.s151515@icloud.com>
2021-02-06 11:49:24 +0900

    guest user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
+  
   root to: 'routes#index'
   resources :routes, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :comments, only: [:create]
